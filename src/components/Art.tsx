0c3028a19fcb21f5afdb7df83cef9142bc870670
Isabelle Darling <91030826+isabelle-d@users.noreply.github.com>
2026-07-04 14:57:14 -0400

    Update Art.tsx

--- src/components/Art.tsx
+++ src/components/Art.tsx
@@ -19,8 +19,8 @@ function Art(){
                 <div title = "Ap art portfolio" style={{ display: "grid", gridTemplateColumns: "1fr 1fr",gap: "10px"}}>
             <img src="/assets/art/Flicks.jpg" style = {{height: "auto", width: "80%"}}></img>
             <img src="/assets/art/Art2.PNG" style = {{height: "auto", width: "80%"}}></img>
-            <img src="/assets/art/Art2.png" style = {{height: "auto", width: "80%"}}></img>
-            <img src="/assets/art/art3.png" style = {{height: "auto", width: "80%"}}></img>
+            <img src="/assets/art/Art1.JPG" style = {{height: "auto", width: "80%"}}></img>
+            <img src="/assets/art/art3.PNG" style = {{height: "auto", width: "80%"}}></img>
             <img src="/assets/art/art4.png" style = {{height: "auto", width: "80%"}}></img>
             <img src="/assets/art/art5.png" style = {{height: "auto", width: "80%"}}></img>
             <img src="/assets/art/art6.jpg" style = {{height: "auto", width: "80%"}}></img>
