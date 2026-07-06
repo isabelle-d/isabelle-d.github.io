f28f2ae55a028b884edcc1a08b43fd55e5bbfaab
isabelle-d <izd1850@gmail.com>
2026-07-06 15:54:44 -0400

    added more art to game art

--- src/components/Art.tsx
+++ src/components/Art.tsx
@@ -1,4 +1,4 @@
-import {  Fieldset,  Tab, Tabs,  Frame } from '@react95/core';
+import {  Fieldset,  Tab, Tabs,  Frame, Video } from '@react95/core';
 
 function Art(){
     return(
@@ -11,19 +11,21 @@ function Art(){
                 <img src="assets/art/sprite2.PNG" style = {{height: "auto", width: "80%"}}></img>
                 <img src="assets/art/for.PNG" style = {{height: "auto", width: "80%"}}></img>
                 <img src="assets/art/hypermaniaMenu.PNG" style = {{height: "auto", width: "80%"}}></img>
+                <img src = "assets/art/healthbar.png" style = {{height: "auto", width: "80%"}}></img>
+                <Video src = "assets/art/ability.mp4" style = {{height: "auto", width: "80%"}}></Video>
 
                 </Fieldset>
              </Frame>
             </Tab>
             <Tab title = "Personal">
             <Frame >
-                <div title = "Ap art portfolio" style={{ display: "grid", gridTemplateColumns: "1fr 1fr",gap: "10px"}}>
+                <div title = "Personal" style={{ display: "grid", gridTemplateColumns: "1fr 1fr",gap: "10px"}}>
             <img src="/assets/art/Flicks.jpg" style = {{height: "auto", width: "80%"}}></img>
             <img src="/assets/art/Art2.PNG" style = {{height: "auto", width: "80%"}}></img>
             <img src="/assets/art/Art1.JPG" style = {{height: "auto", width: "80%"}}></img>
             <img src="/assets/art/art3.PNG" style = {{height: "auto", width: "80%"}}></img>
             <img src="/assets/art/Art4.PNG" style = {{height: "auto", width: "80%"}}></img>
-            <img src="assets/art/art5.PNG" style = {{height: "auto", width: "80%"}}></img>
+            <img src="assets/art/Art5.PNG" style = {{height: "auto", width: "80%"}}></img>
             <img src="assets/art/Art6.jpg" style = {{height: "auto", width: "80%"}}></img>
             <img src="assets/art/art7.PNG" style = {{height: "auto", width: "80%"}}></img>
             <img src="assets/art/art8.PNG" style = {{height: "auto", width: "80%"}}></img>
